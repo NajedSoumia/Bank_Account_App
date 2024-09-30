727f76349b8068d708ce1572edaf5015870fe00e
soumia <soumianajed@gmail.com>
2024-09-30 21:32:24 +0200

    bank account app

---
+++ bank-account-app/customer-service/src/main/java/org/soumia/customerservice/CustomerServiceApplication.java
@@ -0,0 +1,47 @@
+package org.soumia.customerservice;
+
+import org.soumia.customerservice.entities.Customer;
+import org.soumia.customerservice.repository.CustomerRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
+
+
+@SpringBootApplication
+public class CustomerServiceApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(CustomerServiceApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner commandLineRunner(CustomerRepository customerRepository) {
+		return args -> {
+
+			List<Customer> customerList = List.of(
+					Customer.builder()
+							.firstName("Ilyas")
+							.lastName("Elharti")
+							.email("ie@gmail.com")
+							.build(),
+					Customer.builder()
+							.firstName("Soumia")
+							.lastName("Najed")
+							.email("sn@gmail.com")
+							.build(),
+					Customer.builder()
+							.firstName("Hind")
+							.lastName("Najed")
+							.email("hn@gmail.com")
+							.build()
+			);
+
+			customerRepository.saveAll(customerList);
+		};
+	}
+
+}
